feb5e4a0c30e84c4e1410ccf688f9497552e95b7
m-ue-d <fabian.mild@gmx.at>
2022-12-14 21:42:16 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Raylib_CsLo;
+
+public class Konsole
+{
+
+    public static void Main()
+    {
+        Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
+        Raylib.InitWindow(1000, 500, "Konsole");
+        var icon = Raylib.LoadImageFromTexture(Raylib.LoadTexture("resources/Habicht.png"));
+        Raylib.SetWindowIcon(icon);
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            //code
+            Raylib.ClearBackground(Raylib.GOLD);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+        Raylib.UnloadImage(icon);
+    }
+
+}
