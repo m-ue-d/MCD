f4a4a2719a3bebf8cdd0d99a26c54d669da74c16
m-ue-d <fabian.mild@gmx.at>
2022-12-14 23:24:58 +0100

    integrate resources folder easily

--- Program.cs
+++ Program.cs
@@ -2,19 +2,24 @@
 
 using Raylib_CsLo;
 
+namespace Konsole;
+
 public class Konsole
 {
+    const string Resources = @"../../../resources";
 
     public static void Main()
     {
         Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
         Raylib.InitWindow(1000, 500, "Konsole");
-        var icon = Raylib.LoadImageFromTexture(Raylib.LoadTexture("resources/Habicht.png"));
+        var icon = Raylib.LoadImageFromTexture(Raylib.LoadTexture($"{Resources}/Habicht.png"));
         Raylib.SetWindowIcon(icon);
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
-            //code
+            //begin code
+            
+            //end code
             Raylib.ClearBackground(Raylib.GOLD);
             Raylib.EndDrawing();
         }
